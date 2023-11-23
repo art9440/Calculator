d4760aa612c9c45d4665a94319f30afb2c73511b
art9440 <art9440@gmail.com>
2023-11-23 13:46:59 +0700

    made PostFixPolka function, but have error in it

--- Calculator.c
+++ Calculator.c
@@ -6,7 +6,7 @@
 
 
 int isoperand(char sym){
-    char operation[4] = "+-/*()";
+    char operation[4] = "+-/*";
     if (strchr(operation, sym) == NULL)
         return 0;
     return 1;
@@ -24,6 +24,7 @@ int checknear(char first, char second){
     if (first == ")" && second == ")")
         return 1;
 }
+
 
 int priority(char sym){
     switch (sym) {
@@ -48,21 +49,43 @@ char* PostFixPolka(char *calc_str, STACK * calculator){
     post_calc_str = (char*)malloc(sizeof(char));
     int k_num = 0;
     for (int i = 0; i < strlen(calc_str); i++){
-        if (isalpha(calc_str[i]) || isoperand(calc_str[i]) == 0)
+        /*if (isalpha(calc_str[i]) || isoperand(calc_str[i]) == 0)
             return "syntax error";
         if (checknear(calc_str[i], calc_str[i + 1]) == 0)
-            return "syntax error";
+            return "syntax error";*/
         if (isdigit(calc_str[i])) {
             post_calc_str[k_num] == calc_str[i];
             k_num++;
         }
-        if (isoperand(calc_str[i])){
+        if (isoperand(calc_str[i])) {
             k_num++;
             if (empty(calculator) == 0 || get(calculator) == "(")
                 push(calculator, create(calc_str[i]));
+            if (priority(calc_str[i]) > priority(get(calculator)))
+                push(calculator, create(calc_str[i]));
+            if (priority(calc_str[i]) <= priority(get(calculator)))
+                while (priority(calc_str[i]) <= priority(get(calculator))
+                       || get(calculator) != "(") {
+                    post_calc_str[k_num] == pop(calculator);
+                    k_num++;
+                }
         }
+        if (calc_str[i] == "(")
+            push(calculator, create(calc_str[i]));
+        if (calc_str[i] == ")") {
+            while (get(calculator) != "(") {
+                post_calc_str[k_num] = pop(calculator);
+                k_num++;
+            }
+            int bin = pop(calculator);
+        }
+        }
+    while (!empty(calculator)){
+        post_calc_str[k_num] = pop(calculator);
+        k_num ++;
     }
-}
+    }
+
 
 
 int main(){
@@ -72,11 +95,12 @@ int main(){
     calc_str = (char*)malloc(sizeof(char));
     scanf("%s", calc_str);
     char* post_calc_str = PostFixPolka(calc_str, calculator);
-    if (strcmp(post_calc_str, "syntax error")) {
+    /*if (strcmp(post_calc_str, "syntax error")) {
         puts("syntax error");
         return 0;
-    }
-
+    }*/
+    for (int i = 0; i < strlen(post_calc_str); i++)
+        printf("%c", post_calc_str[i]);
 
     return 0;
 }
