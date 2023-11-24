5da9446fedfccef8acec7e67484397ce24f93328
art9440 <art9440@gmail.com>
2023-11-24 21:44:40 +0700

    PostFixPolka is done

--- Calculator.c
+++ Calculator.c
@@ -6,7 +6,7 @@
 
 
 int isoperand(char sym){
-    char operation[4] = "+-/*";
+    char operation[6] = "+-/*()";
     if (strchr(operation, sym) == NULL)
         return 0;
     return 1;
@@ -17,11 +17,11 @@ int checknear(char first, char second){
     if (isoperand(first) && isoperand(second) && strchr("()", first) == NULL
     && strchr("()", second) == NULL)
         return 0;
-    if (first == "(" && second == ")" || first == ")" && second == "(")
+    if (first == '(' && second == ')' || first == ')' && second == '(')
         return 0;
-    if (first == "(" && second == "(")
+    if (first == '(' && second == '(')
         return 1;
-    if (first == ")" && second == ")")
+    if (first == ')' && second == ')')
         return 1;
 }
 
@@ -30,62 +30,72 @@ int priority(char sym){
     switch (sym) {
         case '+':
         case '-':
-            return 1;
+            return 3;
         case '*':
         case '/':
-            return 2;
-        case '(':
-            return 3;
-        case ')':
             return 4;
+        case '(':
+            return 1;
+        case ')':
+            return 2;
+        default:
+            return 0;
     }
-    return 0;
 }
 
 
-char* PostFixPolka(char *calc_str, STACK * calculator){
+char* PostFixPolka(char *calc_str, STACK * calculator) {
     char *post_calc_str;
     int calc_len = strlen(calc_str);
-    post_calc_str = (char*)malloc(sizeof(char));
+    post_calc_str = (char *) malloc((calc_len + 1) * sizeof(char));
     int k_num = 0;
-    for (int i = 0; i < calc_len; i++){
+    char symbol;
+    for (int i = 0; i < calc_len; i++) {
         /*if (isalpha(calc_str[i]) || isoperand(calc_str[i]) == 0)
             return "syntax error";
         if (checknear(calc_str[i], calc_str[i + 1]) == 0)
             return "syntax error";*/
+
         if (isdigit(calc_str[i])) {
-            post_calc_str[k_num] = calc_str[i];
-            k_num++;
+            post_calc_str[k_num++] = calc_str[i];
         }
-        else if (isoperand(calc_str[i]) || calc_str[i]
-        == "(" || calc_str[i] == ")") {
-            if (empty(calculator) == 0 || get(calculator) == "(")
+        else if (isoperand(calc_str[i])) {
+            post_calc_str[k_num++] = ' ';
+
+            if (empty(calculator)) {
                 push(calculator, create(calc_str[i]));
-            if (priority(calc_str[i]) > priority(get(calculator)))
-                push(calculator, create(calc_str[i]));
-            if (priority(calc_str[i]) <= priority(get(calculator)))
-                while (priority(calc_str[i]) <= priority(get(calculator))
-                       || get(calculator) != "(") {
-                    post_calc_str[k_num] = (char) pop(calculator);
-                    k_num++;
-                }
-        }
-        if (calc_str[i] == "(")
-            push(calculator, create(calc_str[i]));
-        if (calc_str[i] == ")") {
-            while (get(calculator) != "(") {
-                post_calc_str[k_num] = (char) pop(calculator);
-                k_num++;
             }
-            int bin = pop(calculator);
+            else {
+                if (calc_str[i] == '(') {
+                    push(calculator, create(calc_str[i]));
+                } else if (calc_str[i] == ')') {
+                    symbol = (char) pop(calculator);
+                    while (symbol != '(') {
+                        post_calc_str[k_num++] = symbol;
+                        symbol = (char) pop(calculator);
+                    }
+                } else if (priority(calc_str[i]) > priority((char) get(calculator))) {
+                    push(calculator, create(calc_str[i]));
+                } else {
+                    while (priority(calc_str[i]) <= priority((char) get(calculator))) {
+                        post_calc_str[k_num++] = (char) pop(calculator);
+                        if (empty(calculator))
+                            break;
+                    }
+                    push(calculator, create(calc_str[i]));
+                }
+            }
         }
-        }
-    while (!empty(calculator)){
-        post_calc_str[k_num] = (char) pop(calculator);
-        k_num ++;
+
     }
+
+    while (!empty(calculator)) {
+        post_calc_str[k_num++] = (char)pop(calculator);
+    }
+
+    post_calc_str[k_num] = '\0';
     return post_calc_str;
-    }
+}
 
 
 
